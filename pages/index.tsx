fe26aef700bdcf0f6e4414158f56886dded46206
Bruno <brun0yuj1ym02@gmail.com>
2023-01-05 00:09:23 +0900

    tailwind installed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,10 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from '@next/font/google';
-import styles from '../styles/Home.module.css';
 import Layout from '../components/Layout';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
       <Layout>
-        <h1>Home</h1>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </Layout>
     </>
   );
