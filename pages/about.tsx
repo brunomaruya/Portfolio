bc2a3bf882e0d7c011b0b379b103d95dbc7ba7c4
Bruno <brun0yuj1ym02@gmail.com>
2023-01-11 12:50:16 +0900

    update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import profileImg from '../public/images/profile.jpg';
 import skillsLogosData from '../data/skillsLogos.json';
+import GitHubCalendar from 'react-github-calendar';
 
 export default function About() {
   return (
@@ -13,7 +14,7 @@ export default function About() {
             <div className="flex gap-10">
               <div>
                 <h1 className="text-5xl mb-4">
-                  Know Who <span className="text-primary-color">I Am</span>
+                  <span className="text-primary-color">Know Who </span>I Am
                 </h1>
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
@@ -36,17 +37,22 @@ export default function About() {
             <div className="flex justify-center flex-wrap gap-x-20 gap-y-10">
               {skillsLogosData.map((skillLogo) => {
                 return (
-                  <div className="" key={skillLogo.id}>
-                    <div
-                      className="flex justify-center items-center border rounded-md
-                    border-primary-color p-10"
-                    >
-                      <i className={`text-7xl ${skillLogo.logoUrl}`}></i>
-                    </div>
+                  <div
+                    key={skillLogo.id}
+                    className="flex justify-center items-center border rounded-md
+                    border-primary-color p-10 hover:scale-105 duration-100"
+                  >
+                    <i className={`text-7xl ${skillLogo.logoUrl}`}></i>
                   </div>
                 );
               })}
             </div>
+          </section>
+          <section className="mt-48 flex flex-col items-center justify-center">
+            <h1 className="text-5xl mb-14 ">
+              Days I&nbsp;<strong className="text-primary-color">Code</strong>
+            </h1>
+            <GitHubCalendar username="brunomaruya" color="rgb(14 165 233)" />
           </section>
         </main>
       </Layout>
