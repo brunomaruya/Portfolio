a4cf4476185ae991ebf0c06f30e36fee08b23cb8
Bruno <brun0yuj1ym02@gmail.com>
2023-01-16 16:52:56 +0900

    update projects.json

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 import Layout from '../components/Layout';
 import projectsData from '../data/projects.json';
@@ -10,10 +11,14 @@ export default function Projects() {
           <h1 className="text-5xl mb-14 flex justify-center text-primary-color">
             Projects
           </h1>
-          <div>
+          <div className="grid grid-cols-3 grid-flow-row gap-10">
             {projectsData.map((project) => (
-              <div key={project.id}>
+              <div
+                key={project.id}
+                className="text-center bg-navbar-on-scroll py-3 px-10 border border-primary-color rounded-md"
+              >
                 <h1>{project.name}</h1>
+                <Image width={40} height={30} src={project.imageUrl} />
               </div>
             ))}
           </div>
