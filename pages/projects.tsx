80ff9903e6b10e9797ab0ac52e59b72033e9546e
Bruno <brun0yuj1ym02@gmail.com>
2023-01-16 16:43:32 +0900

    add projectsData and update Projects Page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import projectsData from '../data/projects.json';
 
 export default function Projects() {
   return (
@@ -9,6 +10,13 @@ export default function Projects() {
           <h1 className="text-5xl mb-14 flex justify-center text-primary-color">
             Projects
           </h1>
+          <div>
+            {projectsData.map((project) => (
+              <div key={project.id}>
+                <h1>{project.name}</h1>
+              </div>
+            ))}
+          </div>
         </section>
       </main>
     </Layout>
